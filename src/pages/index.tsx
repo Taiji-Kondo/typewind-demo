138ee123b0aedcb60dff06438c1a0d5cbe87fd9e
taiji <k.taiji1108@gmail.com>
2023-03-09 20:39:23 +0900

    feat: Creat demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
+import {tw} from "typewind";
+import {useState} from "react";
+import {eventPackageUsedInGetServerSideProps} from "next/dist/telemetry/events";
+
 export default function Home() {
+  const [isError, setIsError] = useState(false)
+  const [text, setText] = useState('')
+
+  const onSubmit = () => {
+    if (text === '') {
+      setIsError(true)
+      console.error('required')
+      return
+    }
+    console.log(text)
+  }
+
+  const onRestError = () => {
+    setIsError(false)
+  }
+
   return (
     <>
-      <h1 className={''}>Typewind Demo</h1>
+      <h1 className={tw.text_lg.text_blue_400}>Typewind Demo</h1>
+      <p className={'text-sm mt-4'}>Simple String</p>
+      <p className={tw.text_sm.mt_4.inset_['1/2']}>Typewind</p>
+      {/* ↓ NG */}
+      <div className={tw.mr_2 + isError ? tw.text_red_500 : tw.text_black}>NG label</div>
+      {/* ↓ OK */}
+      <div className={isError ? tw.text_red_500.mt_2 : tw.text_black.mt_2}>OK label1</div>
+      {/* ↓ OK */}
+      <div className={`${tw.mt_2} ${isError ? tw.text_red_500 : tw.text_black}`}>OK label2</div>
+      <input className={tw.border.border_gray_500} value={text} onChange={(event) => setText(event.currentTarget.value)}/>
+      <button className={tw.block.bg_blue_200.p_2.rounded.mt_1} onClick={onSubmit}>Submit</button>
+      <button className={tw.block.bg_gray_300.p_2.rounded.mt_1} onClick={onRestError}>Reset Error</button>
     </>
   )
 }
